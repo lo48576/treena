a81e5ad047850772ba40af06ab97ce5e4ac6face
YOSHIOKA Takuma <lo48576@hard-wi.red>
2021-09-04 04:44:35 +0900

    Add basic files

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! Treena: Tree stored in an arena.
+#![forbid(unsafe_code)]
+#![warn(rust_2018_idioms)]
+// `clippy::missing_docs_in_private_items` implies `missing_docs`.
+#![warn(clippy::missing_docs_in_private_items)]
+#![warn(clippy::unwrap_used)]
