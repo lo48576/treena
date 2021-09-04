e6e1a0e1121ca62f946e3c7822288db1e7095fba
YOSHIOKA Takuma <lo48576@hard-wi.red>
2021-09-05 05:35:02 +0900

    Support `no_std` environment

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@
 // `clippy::missing_docs_in_private_items` implies `missing_docs`.
 #![warn(clippy::missing_docs_in_private_items)]
 #![warn(clippy::unwrap_used)]
+#![cfg_attr(not(feature = "std"), no_std)]
