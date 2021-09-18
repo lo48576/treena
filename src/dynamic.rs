f8507bdfb0e3338aa8e94381eced563acfc069d9
YOSHIOKA Takuma <lo48576@hard-wi.red>
2021-09-18 20:17:35 +0900

    Move anchor types to a separate module

--- src/dynamic.rs
+++ src/dynamic.rs
@@ -1,50 +1,10 @@
 //! Trees and forests with mutable hierarchy.
 
+mod anchor;
 pub mod forest;
 mod hierarchy;
 mod id;
 
+pub use self::anchor::{AdoptAs, InsertAs};
 pub use self::forest::Forest;
 pub use self::id::NodeId;
-
-/// Relation of the node being `adopt`ed.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum AdoptAs {
-    /// As the first child.
-    FirstChild,
-    /// As the last child.
-    LastChild,
-    /// As the previous sibling.
-    PreviousSibling,
-    /// As the next sibling.
-    NextSibling,
-}
-
-impl AdoptAs {
-    /// Creates `InsertAs` with the given anchor.
-    #[must_use]
-    fn insert_with_anchor(self, anchor: NodeId) -> InsertAs {
-        match self {
-            Self::FirstChild => InsertAs::FirstChildOf(anchor),
-            Self::LastChild => InsertAs::LastChildOf(anchor),
-            Self::PreviousSibling => InsertAs::PreviousSiblingOf(anchor),
-            Self::NextSibling => InsertAs::NextSiblingOf(anchor),
-        }
-    }
-}
-
-/// Target destination to insert, append, or prepend a node.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-// All variants have the common suffix "Of", but this is intended.
-// Variants would be used as, for example, `InsertAs::FirsChildOf(some_node)`.
-#[allow(clippy::enum_variant_names)]
-pub enum InsertAs {
-    /// As the first child.
-    FirstChildOf(NodeId),
-    /// As the last child.
-    LastChildOf(NodeId),
-    /// As the previous sibling.
-    PreviousSiblingOf(NodeId),
-    /// As the next sibling.
-    NextSiblingOf(NodeId),
-}
