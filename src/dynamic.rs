4a3c84b33126f5f4ddfb57b10002e806f52da89d
YOSHIOKA Takuma <lo48576@hard-wi.red>
2021-09-26 23:46:41 +0900

    Export public iterator types for forest

--- src/dynamic.rs
+++ src/dynamic.rs
@@ -6,7 +6,10 @@ mod hierarchy;
 mod id;
 
 pub use self::anchor::{AdoptAs, InsertAs};
-pub use self::forest::traverse::{Ancestors, DepthFirstTraverse, DftEvent, Siblings};
+pub use self::forest::traverse::{
+    Ancestors, BreadthFirstTraverse, DepthFirstTraverse, DftEvent, ShallowDepthFirstTraverse,
+    Siblings,
+};
 #[cfg(any(feature = "debug-print"))]
 pub use self::forest::DebugPrint;
 pub use self::forest::{Forest, Node, NodeMut, StructureError, TreeBuilder};
