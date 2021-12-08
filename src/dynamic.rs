a5534807588b7a5164726ef9933a65de31ce363e
YOSHIOKA Takuma <lo48576@hard-wi.red>
2021-12-09 06:25:06 +0900

    Export `DebugPrint` unconditionally from `dynamic` module

--- src/dynamic.rs
+++ src/dynamic.rs
@@ -84,9 +84,7 @@ pub use self::forest::traverse::{
     AllocatingBreadthFirstTraverse, Ancestors, BreadthFirstTraverse, DepthFirstTraverse, DftEvent,
     ShallowDepthFirstTraverse, Siblings,
 };
-#[cfg(any(feature = "debug-print"))]
-pub use self::forest::DebugPrint;
-pub use self::forest::{Forest, Node, NodeMut, StructureError, TreeBuilder};
+pub use self::forest::{DebugPrint, Forest, Node, NodeMut, StructureError, TreeBuilder};
 use self::id::NodeIdExt;
 pub use self::id::{
     InternalNodeId, NodeId, NodeIdU16, NodeIdU32, NodeIdU64, NodeIdU8, NodeIdUsize,
